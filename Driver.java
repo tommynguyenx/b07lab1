29b46aa696621c43dfba493914e72e8c2cf68930
Tommy Nguyen <haidangasian@gmail.com>
2024-09-26 19:46:56 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,57 @@
+import java.util.Arrays;
+import java.io.File;
+
 public class Driver {
 	public static void main(String [] args) {
+		// Testing Polynomial constructors, .add, .evaluate and .hasRoot
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
+		double [] c1_coef = {6, 5};
+		int [] c1_exp = {0, 3};
+		Polynomial p1 = new Polynomial(c1_coef, c1_exp);
+		double [] c2_coef = {-2,-9};
+		int [] c2_exp = {1, 4};
+		Polynomial p2 = new Polynomial(c2_coef, c2_exp);
+		Polynomial s1 = p1.add(p2);
+		System.out.println("s1(0.1) = " + s1.evaluate(0.1));
+		if(s1.hasRoot(1))
+			System.out.println("1 is a root of s1");
 		else
-			System.out.println("1 is not a root of s");
+			System.out.println("1 is not a root of s1");
+		
+		
+		// testing Polynomial.multiple, .add
+		
+		double [] c3_coef = {2,-7,1,-5,3};
+		int [] c3_exp = {2,0,1,3,4};
+		Polynomial p3 = new Polynomial(c3_coef, c3_exp);
+		double [] c4_coef = {9,-1,5,-2};
+		int [] c4_exp = {0,1,3,5};
+		Polynomial p4 = new Polynomial(c4_coef, c4_exp);
+		
+		Polynomial s2 = p3.add(p4);
+		System.out.println("(coefs) sum of p3 and p4 is: " + Arrays.toString(s2.coef));
+		System.out.println("(exp) sum of p3 and p4 is: " + Arrays.toString(s2.exp));
+		
+		Polynomial product = p3.multiply(p4);
+		System.out.println("(coefs) product of p3 and p4 is: " + Arrays.toString(product.coef));
+		System.out.println("(exp) product of p3 and p4 is: " + Arrays.toString(product.exp));
+		
+		
+		// Testing constructor using File
+		//String str = new String("5-3x2+7x8");
+		// "(?=[+-])" check if for any given position, does + or - follows it, if yes then split around
+		// System.out.println(Arrays.toString(str.split("(?=[+-])")));
+		
+		File f = new File("C:/Users/thuan/Downloads/lab2poly/src/lab2poly/polysample.txt/");
+		Polynomial p5 = new Polynomial(f);
+		System.out.println("(coefs) poly read from file: " + Arrays.toString(p5.coef));
+		System.out.println("(exp) poly read from file: " + Arrays.toString(p5.exp));
+		
+		
+		// testing .saveToFile
+		String pathPolyToSave = "C:/Users/thuan/Downloads/lab2poly/src/lab2poly/polypaste.txt/";
+		product.saveToFile(pathPolyToSave);
+		
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,38 +1,147 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Polynomial {
 	double [] coef;
+	int [] exp;
 	
 	public Polynomial() {
-		this.coef = new double[1];
+		this.coef = new double[0];
+		this.exp = new int[0];
+	}
+	
+	public Polynomial(double [] coef, int [] exp) {
+		int len = coef.length;
+
+		this.coef = new double[len];
+		this.exp = new int[len];
+		for (int i = 0; i < len; ++i) {
+			this.coef[i] = coef[i];
+			this.exp[i] = exp[i];
+		}
+	}
+	
+	public Polynomial(File f) {
+		try (Scanner x = new Scanner(f)) {
+			ArrayList<Double> x_coef_arrl = new ArrayList<Double>(0);
+			ArrayList<Integer> x_exp_arrl = new ArrayList<Integer>(0);
+			
+			String input = new String(x.nextLine());
+			x.close();
+			for (String term : input.split("(?=[+-])")) {
+				String [] components = term.split("x");
+				x_coef_arrl.add(Double.parseDouble(components[0]));
+				if (term.contains("x") && components.length == 2) {
+					x_exp_arrl.add(Integer.parseInt(components[1]));
+				} else if (term.contains("x")) {
+					x_exp_arrl.add(1);
+				} else {
+					x_exp_arrl.add(0);
+				}
+			}
+			
+			int len = x_coef_arrl.size();
+			this.coef = new double[len];
+			this.exp = new int[len];
+			if (len > 0) {
+				for (int i = 0; i < len; ++i) {
+					this.coef[i] = x_coef_arrl.get(i);
+					this.exp[i] = x_exp_arrl.get(i);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// (have try-catch here so that Eclipse don't flag)
+		}
+		
 	}
 
-	public Polynomial(double [] coef) {
-		this.coef = new double[coef.length];
-		for (int i = 0; i < coef.length; ++i) {
-			this.coef[i] = coef[i];
+	public HashMap<Integer, Double> makeHash() {
+		HashMap<Integer, Double> polyHash = new HashMap<Integer, Double>();
+
+		for (int i = 0; i < this.coef.length; ++i) {
+			polyHash.put(this.exp[i], this.coef[i]);
 		}
+		return polyHash;
+	}
+	
+	public Polynomial convertHashToPoly(HashMap<Integer, Double> xHash) {
+		ArrayList<Double> x_coef_arrl = new ArrayList<Double>(0);
+		ArrayList<Integer> x_exp_arrl = new ArrayList<Integer>(0);
+		
+		for (int key : xHash.keySet()) {
+			if (xHash.get(key) != 0) {
+				x_exp_arrl.add(key);
+				x_coef_arrl.add(xHash.get(key));
+			}
+		}
+		
+		int len = x_exp_arrl.size();
+		Polynomial x;
+		
+		if (len == 0) {
+			x = new Polynomial();
+		} else {
+			double[] new_coef = new double[len];
+			int[] new_exp = new int[len];
+			for (int i = 0; i < len; ++i) {
+				new_coef[i] = x_coef_arrl.get(i);
+				new_exp[i] = x_exp_arrl.get(i);
+			}
+			x = new Polynomial(new_coef, new_exp);
+		}
+		return x;
 	}
 
 	public Polynomial add(Polynomial p) {
-		double [] x = new double[Math.max(p.coef.length, coef.length)];
+		HashMap<Integer, Double> pHash = p.makeHash();
+		HashMap<Integer, Double> objHash = this.makeHash();
+		HashMap<Integer, Double> xHash = new HashMap<Integer, Double>();
 		
-		for (int i = 0; i < x.length; ++i) {
-			if (i < Math.min(p.coef.length, coef.length))
-				x[i] = p.coef[i] + coef [i];
-			else if (p.coef.length > coef.length)
-				x[i] = p.coef[i];
-			else
-				x[i] = coef[i];
+		for (int exp : objHash.keySet()) {
+			if (pHash.containsKey(exp)) {
+				xHash.put(exp, objHash.get(exp)+pHash.get(exp));
+			} else {
+				xHash.put(exp, objHash.get(exp));
+			}
 		}
-
-		Polynomial q = new Polynomial(x);
-		return q;
+		for (int exp : pHash.keySet()) {
+			if (!xHash.containsKey(exp)) {
+				xHash.put(exp, pHash.get(exp));
+			}
+		}
+		return convertHashToPoly(xHash);
 	}
 
 
+	public Polynomial multiply(Polynomial p) {
+		HashMap<Integer, Double> objHash = this.makeHash();
+		HashMap<Integer, Double> pHash = p.makeHash();
+		HashMap<Integer, Double> xHash = new HashMap<Integer, Double>();
+		
+		for (int exp1 : objHash.keySet()) {
+			for (int exp2 : pHash.keySet()) {
+				int new_exp = exp1 + exp2;
+				if (!xHash.containsKey(new_exp)) {
+					xHash.put(new_exp, objHash.get(exp1)*pHash.get(exp2));
+				} else {
+					xHash.replace(new_exp, xHash.get(new_exp) + objHash.get(exp1)*pHash.get(exp2));
+				}
+			}
+		}
+		return convertHashToPoly(xHash);
+	}
+	
+
 	public double evaluate(double x) {
 		double val = 0;
-		for (int i = 0; i < coef.length; ++i) 
-			val += coef[i]*(Math.pow(x, i));
+		for (int i = 0; i < this.coef.length; ++i) 
+			val += this.coef[i]*(Math.pow(x, this.exp[i]));
 
 		return val;
 	}
@@ -41,6 +150,32 @@ public class Polynomial {
 	public boolean hasRoot(double x) {
 		return evaluate(x) == 0;
 	}
-
-}
 	
+	public void saveToFile(String path) {
+		// assume given file is empty
+		try (FileWriter output = new FileWriter(new File(path))) {
+			int len = this.coef.length;
+			String polyStr = new String();
+			
+			for (int i = 0; i < len; ++i) {
+				String toWrite = new String();
+
+				if (this.coef[i] != 1)
+					toWrite += String.valueOf(this.coef[i]);
+				if (i != 0 && this.coef[i] > 0)
+					toWrite = "+" + toWrite;
+				if (this.exp[i] > 0) {
+					toWrite += "x";
+					if (this.exp[i] > 1)
+						toWrite += String.valueOf(this.exp[i]);
+				}
+				polyStr += toWrite;
+			}
+			
+			output.write(polyStr);
+			output.close();
+		} catch (IOException e) {
+			// (try-catch for Eclipse to not flag) assume valid inputs
+		}
+	}
+}
